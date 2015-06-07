989d0f799a036dc09445510e85f8c3a9397d7589
Aayush Subedi <subedi.aayush@gmail.com>
2015-06-07 18:54:50 +0545

    Register Form

---
+++ rango/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Category, Page, UserProfile
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title', 'url')
+
+# Register your models here.
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
