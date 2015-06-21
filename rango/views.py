b8d3f3b76f6cf45324d6ae650a405f0007ccb3b6
subediaayush <subediaayush@gmail.com>
2015-06-21 19:43:09 +0545

    Some Bootstrap elements

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import Category, Page
 from .forms import CategoryForm, PageForm
 from .forms import UserForm, UserProfileForm
@@ -7,19 +11,47 @@ from .forms import UserForm, UserProfileForm
 
 
 def about(request):
-    return render(request, 'rango/about.html')
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    return render(request, 'rango/about.html', {'visits': count})
 
 
 def index(request):
-    context_dict = {}
 
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
 
-    context_dict['categories'] = category_list
-    context_dict['pages'] = page_list
+    context_dict = {'categories': category_list, 'pages': page_list}
 
-    return render(request, 'rango/index.html', context_dict)
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
 
 
 def category(request, category_name_slug):
@@ -82,7 +114,10 @@ def add_page(request, category_name_slug):
 
 
 def register(request):
-    registered = False
+    if request.session.test_cookie_worked():
+        print(">>>> TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+        registered = False
 
     if request.method == 'POST':
         user_form = UserForm(data=request.POST)
@@ -114,3 +149,42 @@ def register(request):
     return render(request,
                   'rango/register.html',
                   {'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/rango/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html', {})
+
+
+@login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
+def user_logout(request):
+    # Since we know the user is logged in, we can now just log them out.
+    logout(request)
+
+    # Take the user back to the homepage.
+    return HttpResponseRedirect('/rango/')
--- twdp/urls.py
+++ twdp/urls.py
@@ -1,10 +1,22 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+# Create a new class that redirects the user to the index page, if successful at logging
+
+
+class MyRegistrationView(RegistrationView):
+
+    def get_success_url(self, request, user):
+        return '/rango/'
+
 
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^rango/', include('rango.urls')),
+                       url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+                       (r'^accounts/', include('registration.backends.simple.urls')),
                        )
 
 if settings.DEBUG:
